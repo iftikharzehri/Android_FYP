d1256e8498a4b80b677d230da69740b4f60446d9
iftikharzehri <iftikharahmed111000@gmail.com>
2023-01-04 13:52:53 +0500

    splash screen

--- app/src/main/java/com/example/android_fyp/MainActivity.java
+++ app/src/main/java/com/example/android_fyp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.android_fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
